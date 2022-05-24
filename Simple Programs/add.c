8707413456d97d42b8f15f38e267488ab603ceb3
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-24 18:27:53 +0530

    Create add.c

---
+++ Simple Programs/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a=90;
+    int b,c;
+    printf("Enter the values of a and b is:%d\n");
+    scanf("%d %d",&a, &b);
+    c=a+b;
+    printf("\n sum of two numbers %d\n",c);
+     return 0;
+}
