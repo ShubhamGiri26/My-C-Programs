2c3f1ecbf131f7dc2fd53a85b18e7aa852c4b0c6
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-24 18:23:13 +0530

    Update add.c

---
+++ Simple Programs/add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a=90;
+    int b,c;
+    printf("value of a is %d\n",a);
+    printf("value of b is :\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\n %d + %d= %d\n",a,b,c);
+     return 0;
+}
