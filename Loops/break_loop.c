68f08b08ca6ee1ef0c805050a74d55ab366f56ea
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-24 17:37:09 +0530

    Add files via upload

---
+++ Loops/break_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+   int i=0;
+    do{
+        printf("The value of i is %d",i);
+        if(i==4){
+            break;
+        }
+    
+    }
+    while(i<10); 
+    return 0;
+}
---
+++ Loops/loop _intro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    printf("hello");
+    printf("world");
+    int a= 1;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++j;
+    printf("%d\n", a);
+
+    return 0;
+}
