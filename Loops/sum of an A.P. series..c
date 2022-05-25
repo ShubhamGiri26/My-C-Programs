cf8072bbd2147d68ff0efb23b2794565d8a31667
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-26 00:04:39 +0530

    Create sum of an A.P. series..c

---
+++ Loops/sum of an A.P. series..c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main()
+{
+     int a, d, n, i, S;
+     int sum = 0;
+ 
+     printf("Enter the first term value of the A.P. series: ");
+     scanf("%d", &a);
+     
+     printf("Enter the total numbers in the A.P. series: ");
+     scanf("%d", &n);
+     
+     printf("Enter the common difference of A.P. series: ");
+     scanf("%d", &d);
+     sum = (n * (2 * a + (n - 1)* d ))/ 2;
+     S = a + (n - 1) * d;
+     
+     printf("Sum of the A.P series is: ");
+     for (i = a; i <= S; i = i + d )
+     {
+          if (i != S)
+               printf("%d + ", i);
+          else
+               printf("%d = %d ", i, sum);
+     }
+     return 0;
+}
