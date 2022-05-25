dcde2cea8d2590bb245638829bc7a5eec65f3b4f
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-25 23:17:34 +0530

    Create check whether a given number is an armstrong number or not.c

---
+++ Loops/check whether a given number is an armstrong number or not.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int orgNum, num, lastDigit, digits, sum;
+
+    printf("Enter any number to check Armstrong number: ");
+    scanf("%d", &num);
+
+    sum = 0;
+
+    orgNum = num;
+
+    digits = (int) log10(num) + 1;
+
+    while(num > 0)
+    {
+    
+        lastDigit = num % 10;
+
+        sum = sum + round(pow(lastDigit, digits));
+
+           num = num / 10;
+    }
+
+    if(orgNum == sum)
+    {
+        printf("%d is ARMSTRONG NUMBER", orgNum);
+    }
+    else
+    {
+        printf("%d is NOT ARMSTRONG NUMBER", orgNum);
+    }
+
+    return 0;
+}
