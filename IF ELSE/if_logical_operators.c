4c4733f7f5b1014b69cde405dfac55ae494ab469
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-05-28 15:18:25 +0530

    Update if_logical_operators.c

--- IF ELSE/if_logical_operators.c
+++ IF ELSE/if_logical_operators.c
@@ -3,12 +3,11 @@
 int main(){
     int age;
     int vipPass = 0;
-    // vipPass = 1;
-
+   
     printf("Enter your age\n");
     scanf("%d", &age); 
 
-    // if (age <= 70 && age>=18)
+   
     if ((age <= 70 && age>=18) || (vipPass==1))
     {
         printf("You are above 18 and below 70, you can drive\n");
