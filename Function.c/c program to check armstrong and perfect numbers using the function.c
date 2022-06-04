22d76fd6a41fcf8c829fa141f81b6a89b38a8583
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-06-04 17:24:25 +0530

    Create c program to check armstrong and perfect numbers using the function.c

---
+++ Function.c/c program to check armstrong and perfect numbers using the function.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+int isArmstrong(int num);
+int isPerfect(int num);
+
+
+int main()
+{
+    int num;
+    
+    // Call isArmstrong() function
+    if(isArmstrong(num))
+    {
+        printf("%d is Armstrong number.\n", num);
+    }
+    else
+    {
+        printf("%d is not Armstrong number.\n", num);
+    }
+    
+    // Call isPerfect() function
+    if(isPerfect(num))
+    {
+        printf("%d is Perfect number.\n", num);
+    }
+    else
+    {
+        printf("%d is not Perfect number.\n", num);
+    }
+    
+    return 0;
+}
